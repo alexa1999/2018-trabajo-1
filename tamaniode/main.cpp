c8084147c41e8df58d9e977aa85cf792258ae4ff
alexa1999 <anel.veliz@ucsp.edu.pe>
2018-04-14 19:27:22 -0500

    Add files via upload

---
+++ tamaniode/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout<<"int: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"char: "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"short "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"long "<<sizeof(long)<<" bytes"<<endl;
+    cout<<"long long "<<sizeof(long long)<<" bytes"<<endl;
+    cout<<"float "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"double "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"bool "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"unsigned "<<sizeof(unsigned)<<" bytes"<<endl;
+
+    cout<<"long double "<<sizeof(long double)<<" bytes"<<endl;
+    int potencia = pow(9,2000);
+    short potenc = pow(9,2000);
+    long pot =pow (9,900);
+    long long po=pow(9,900);
+    float y=91084093840923841849/894789;
+    double k=potencia /54;
+
+
+
+    //char a[255];
+    //a="23451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345123451234512345";
+    cout<<"int: "<<potencia<<endl;
+    cout<< potenc<<endl;
+    cout<< pot<<endl;
+
+
+
+  return 0;
+}
